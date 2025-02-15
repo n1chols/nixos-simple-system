461e68ebb9fa3a615aa7ea418e8c0f223ccb0b23
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 01:47:42 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -140,3 +140,108 @@
       # Feature configurations
       featureModules = [
         (lib.mkIf disableNixApps {
+          documentation.nixos.enable = false;
+          services.xserver.excludePackages = [ pkgs.xterm ];
+          environment.defaultPackages = [];
+        })
+
+        (lib.mkIf animateStartup {
+          boot.plymouth = {
+            enable = true;
+            theme = "spinner";
+          };
+        })
+
+        (lib.mkIf autoUpgrade {
+          system.autoUpgrade = {
+            enable = true;
+            allowReboot = false;
+            dates = "04:00";
+          };
+        })
+
+        (lib.mkIf gamingTweaks {
+          boot = {
+            kernelPackages = pkgs.linuxPackages_xanmod;
+            kernel.sysctl = {
+              "vm.swappiness" = 10;
+              "vm.vfs_cache_pressure" = 50;
+              "kernel.sched_autogroup_enabled" = 0;
+            };
+            kernelParams = [ "mitigations=off" "nowatchdog" ];
+          };
+        })
+
+        (lib.mkIf hiResAudio {
+          security.rtkit.enable = true;
+          services = {
+            pulseaudio.enable = false;
+            pipewire = {
+              enable = true;
+              alsa = {
+                enable = true;
+                support32Bit = true;
+              };
+              pulse.enable = true;
+              extraConfig.pipewire."context.properties" = {
+                "default.clock.allowed-rates" = [
+                  44100 48000 88200 96000 176400 192000
+                ];
+              };
+            };
+          };
+        })
+
+        (lib.mkIf dualBoot {
+          time.hardwareClockInLocalTime = true;
+          boot.loader.grub.useOSProber = true;
+        })
+
+        (lib.mkIf touchpad {
+          services.xserver.libinput = {
+            enable = true;
+            touchpad = {
+              tapping = true;
+              naturalScrolling = true;
+            };
+          };
+        })
+
+        (lib.mkIf bluetooth {
+          hardware.bluetooth = {
+            enable = true;
+            powerOnBoot = true;
+          };
+          services.blueman.enable = true;
+        })
+
+        (lib.mkIf printing {
+          services = {
+            printing.enable = true;
+            avahi = {
+              enable = true;
+              nssmdns4 = true;
+              openFirewall = true;
+            };
+          };
+        })
+
+        (lib.mkIf battery {
+          services = {
+            tlp.enable = true;
+            upower.enable = true;
+          };
+        })
+      ];
+
+    in lib.nixosSystem {
+      inherit system;
+      modules = [
+        baseSystemConfig
+        cpuConfig
+        gpuConfig
+        bootConfig
+      ] ++ featureModules ++ extraModules;
+    };
+  };
+}
