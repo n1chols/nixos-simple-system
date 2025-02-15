152d38d401569c945dd039ece4c90042f90ac290
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-14 23:52:32 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
             enable = true;
             enable32Bit = true;
             extraPackages = nixpkgs.lib.mkIf (gpuVendor == "intel") [
-              nixpkgs.packages.intel-media-driver
+              nixpkgs.pkgs.intel-media-driver
             ];
           };
           boot = {
