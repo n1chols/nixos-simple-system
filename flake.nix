79c281ceb150fc69744d36d6f57796bfa46f75a7
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 01:06:10 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -44,6 +44,77 @@
             auto-optimise-store = true;
             warn-dirty = false;
           };
+
+          # System Config
+          time.timeZone = timeZone;
+          i18n.defaultLocale = locale;
+          console.keyMap = keyLayout;
+          networking = {
+            hostName = hostName;
+            networkmanager.enable = true;
+          };
+          users.users.${userName} = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" "networkmanager" ];
+          };
+
+          # Hardware Config
+          hardware = {
+            enableAllFirmware = true;
+            enableRedistributableFirmware = true;
+            cpu = nixpkgs.lib.mkMerge [
+              (nixpkgs.lib.mkIf (cpuVendor == "intel") {
+                intel.updateMicrocode = true;
+              })
+              (nixpkgs.lib.mkIf (cpuVendor == "amd") {
+                amd.updateMicrocode = true;
+              })
+            ];
+            #graphics = nixpkgs.lib.mkMerge [{
+            #  enable = true;
+            #  enable32Bit = true;
+            #} (nixpkgs.lib.mkIf (gpuVendor == "intel") {
+            #  extraPackages = [ nixpkgs.legacyPackages.${systemType}.intel-media-driver ];
+            #})];
+            amdgpu = nixpkgs.lib.mkIf (gpuVendor == "amd") {
+              enable = true;
+              amdvlk = true;
+              loadInInitrd = true;
+            };
+            nvidia = nixpkgs.lib.mkIf (gpuVendor == "nvidia") {
+              open = false;
+              nvidiaSettings = true;
+              modesetting.enable = true;
+              package = nixpkgs.legacyPackages.${systemType}.linuxPackages.nvidiaPackages.stable;
+            };
+          };
+
+          boot = {
+            loader = if bootDevice != "" then {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            } else {
+              grub = {
+                enable = true;
+                device = rootDevice;
+                efiSupport = false;
+              };
+            };
+          };
+          fileSystems = {
+            "/" = {
+              device = rootDevice;
+              fsType = "ext4";
+            };
+          } // nixpkgs.lib.optionalAttrs (bootDevice != "") {
+            "/boot" = {
+              device = bootDevice;
+              fsType = "vfat";
+            };
+          };
+          swapDevices = nixpkgs.lib.optional (swapDevice != "") {
+            device = swapDevice;
+          };
         }
       ] ++ extraModules;
     };
