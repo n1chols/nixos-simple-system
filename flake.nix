b33c9bfce9da72a61a7a19e4590b907e1d304fd7
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-15 01:47:06 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -36,6 +36,51 @@
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
 
+      # Hardware-specific configurations
+      cpuConfig = {
+        hardware = {
+          enableAllFirmware = true;
+          enableRedistributableFirmware = true;
+          cpu = lib.mkMerge [
+            (lib.mkIf (cpuVendor == "intel") {
+              intel.updateMicrocode = true;
+            })
+            (lib.mkIf (cpuVendor == "amd") {
+              amd.updateMicrocode = true;
+            })
+          ];
+        };
+      };
+
+      gpuConfig = {
+        hardware = lib.mkMerge [
+          {
+            graphics = {
+              enable = true;
+              enable32Bit = true;
+            };
+          }
+          (lib.mkIf (gpuVendor == "intel") {
+            graphics.extraPackages = [ pkgs.intel-media-driver ];
+          })
+          (lib.mkIf (gpuVendor == "amd") {
+            amdgpu = {
+              enable = true;
+              amdvlk = true;
+              loadInInitrd = true;
+            };
+          })
+          (lib.mkIf (gpuVendor == "nvidia") {
+            nvidia = {
+              open = false;
+              nvidiaSettings = true;
+              modesetting.enable = true;
+              package = pkgs.linuxPackages.nvidiaPackages.stable;
+            };
+          })
+        ];
+      };
+
       # System configurations
       baseSystemConfig = {
         system.stateVersion = "24.11";
@@ -59,47 +104,6 @@
         users.users.${userName} = {
           isNormalUser = true;
           extraGroups = [ "wheel" "networkmanager" ];
-        };
-      };
-
-      # Hardware-specific configurations
-      cpuConfig = lib.mkMerge [
-        {
-          hardware.enableAllFirmware = true;
-          hardware.enableRedistributableFirmware = true;
-        }
-        (lib.mkIf (cpuVendor == "intel") {
-          hardware.cpu.intel.updateMicrocode = true;
-        })
-        (lib.mkIf (cpuVendor == "amd") {
-          hardware.cpu.amd.updateMicrocode = true;
-        })
-      ];
-
-      gpuConfig = {
-        hardware = {
-          graphics = lib.mkMerge [
-            {
-              enable = true;
-              enable32Bit = true;
-            }
-            (lib.mkIf (gpuVendor == "intel") {
-              extraPackages = [ pkgs.intel-media-driver ];
-            })
-          ];
-        } // lib.mkIf (gpuVendor == "amd") {
-          amdgpu = {
-            enable = true;
-            amdvlk = true;
-            loadInInitrd = true;
-          };
-        } // lib.mkIf (gpuVendor == "nvidia") {
-          nvidia = {
-            open = false;
-            nvidiaSettings = true;
-            modesetting.enable = true;
-            package = pkgs.linuxPackages.nvidiaPackages.stable;
-          };
         };
       };
 
@@ -136,108 +140,3 @@
       # Feature configurations
       featureModules = [
         (lib.mkIf disableNixApps {
-          documentation.nixos.enable = false;
-          services.xserver.excludePackages = [ pkgs.xterm ];
-          environment.defaultPackages = [];
-        })
-
-        (lib.mkIf animateStartup {
-          boot.plymouth = {
-            enable = true;
-            theme = "spinner";
-          };
-        })
-
-        (lib.mkIf autoUpgrade {
-          system.autoUpgrade = {
-            enable = true;
-            allowReboot = false;
-            dates = "04:00";
-          };
-        })
-
-        (lib.mkIf gamingTweaks {
-          boot = {
-            kernelPackages = pkgs.linuxPackages_xanmod;
-            kernel.sysctl = {
-              "vm.swappiness" = 10;
-              "vm.vfs_cache_pressure" = 50;
-              "kernel.sched_autogroup_enabled" = 0;
-            };
-            kernelParams = [ "mitigations=off" "nowatchdog" ];
-          };
-        })
-
-        (lib.mkIf hiResAudio {
-          security.rtkit.enable = true;
-          services = {
-            pulseaudio.enable = false;
-            pipewire = {
-              enable = true;
-              alsa = {
-                enable = true;
-                support32Bit = true;
-              };
-              pulse.enable = true;
-              extraConfig.pipewire."context.properties" = {
-                "default.clock.allowed-rates" = [
-                  44100 48000 88200 96000 176400 192000
-                ];
-              };
-            };
-          };
-        })
-
-        (lib.mkIf dualBoot {
-          time.hardwareClockInLocalTime = true;
-          boot.loader.grub.useOSProber = true;
-        })
-
-        (lib.mkIf touchpad {
-          services.xserver.libinput = {
-            enable = true;
-            touchpad = {
-              tapping = true;
-              naturalScrolling = true;
-            };
-          };
-        })
-
-        (lib.mkIf bluetooth {
-          hardware.bluetooth = {
-            enable = true;
-            powerOnBoot = true;
-          };
-          services.blueman.enable = true;
-        })
-
-        (lib.mkIf printing {
-          services = {
-            printing.enable = true;
-            avahi = {
-              enable = true;
-              nssmdns4 = true;
-              openFirewall = true;
-            };
-          };
-        })
-
-        (lib.mkIf battery {
-          services = {
-            tlp.enable = true;
-            upower.enable = true;
-          };
-        })
-      ];
-
-    in lib.nixosSystem {
-      inherit system;
-      modules = [
-        baseSystemConfig
-        cpuConfig
-        gpuConfig
-        bootConfig
-      ] ++ featureModules ++ extraModules;
-    };
-  };
-}
