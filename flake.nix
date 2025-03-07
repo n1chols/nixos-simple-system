f3519e5d2d5eb9fdbad87fb442a4bb09ef968c40
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-06 20:39:47 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -36,7 +36,6 @@
         system.stateVersion = "24.11";
         
         nixpkgs.config.allowUnfree = true;
-
         nix.settings = {
           experimental-features = [ "nix-command" "flakes" ];
           auto-optimise-store = true;
