22c6640c76599ff47e51ea914e644dde2b12c542
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-06 20:38:55 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,11 +27,12 @@
     extraModules ? []
   }: let
     lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${systemType};
   in nixpkgs.lib.nixosSystem {
     system = systemType;
     modules = [
       # Mandatory base configuration
-      ({ pkgs, ... }: {
+      ({ ... }: {
         system.stateVersion = "24.11";
         
         nixpkgs.config.allowUnfree = true;
@@ -104,122 +105,4 @@
 
       # Boot Configurations
       (lib.mkIf (bootDevice != null) {
-        boot.loader.systemd-boot = {
-          enable = true;
-          configurationLimit = 10;
-        };
-        boot.loader.efi = {
-          canTouchEfiVariables = true;
-          efiSysMountPoint = "/boot";
-        };
-        fileSystems."/boot" = {
-          device = bootDevice;
-          fsType = "vfat";
-        };
-      })
-
-      (lib.mkIf (bootDevice == null) {
-        boot.loader.grub = {
-          enable = true;
-          devices = [ rootDevice ];
-          efiSupport = false;
-        };
-      })
-
-      (lib.mkIf (swapDevice != null) {
-        swapDevices = [{ device = swapDevice; }];
-      })
-
-      # Optional Features
-      (lib.mkIf disableNixApps {
-        documentation.nixos.enable = false;
-        services.xserver.excludePackages = [ pkgs.xterm ];
-        environment.defaultPackages = [];
-      })
-
-      (lib.mkIf animateStartup {
-        boot.plymouth = {
-          enable = true;
-          theme = "spinner";
-        };
-      })
-
-      (lib.mkIf autoUpgrade {
-        system.autoUpgrade = {
-          enable = true;
-          allowReboot = false;
-          dates = "04:00";
-        };
-      })
-
-      (lib.mkIf gamingTweaks {
-        boot = {
-          kernelPackages = pkgs.linuxPackages_xanmod;
-          kernel.sysctl = {
-            "vm.swappiness" = 10;
-            "vm.vfs_cache_pressure" = 50;
-            "kernel.sched_autogroup_enabled" = 0;
-          };
-          kernelParams = [ "mitigations=off" "nowatchdog" ];
-        };
-      })
-
-      (lib.mkIf hiResAudio {
-        services.pulseaudio.enable = false;
-        security.rtkit.enable = true;
-        services.pipewire = {
-          enable = true;
-          alsa.enable = true;
-          alsa.support32Bit = true;
-          pulse.enable = true;
-          extraConfig.pipewire = {
-            "context.properties" = {
-              "default.clock.allowed-rates" = [ 44100 48000 88200 96000 176400 192000 ];
-            };
-          };
-        };
-      })
-
-      (lib.mkIf dualBoot {
-        time.hardwareClockInLocalTime = true;
-        boot.loader.grub.useOSProber = true;
-      })
-
-      (lib.mkIf gamepad {
-        hardware.xpadneo.enable = true;
-      })
-
-      (lib.mkIf touchpad {
-        services.xserver.libinput = {
-          enable = true;
-          touchpad = {
-            tapping = true;
-            naturalScrolling = true;
-          };
-        };
-      })
-
-      (lib.mkIf bluetooth {
-        hardware.bluetooth = {
-          enable = true;
-          powerOnBoot = true;
-        };
-        services.blueman.enable = true;
-      })
-
-      (lib.mkIf printing {
-        services.printing.enable = true;
-        services.avahi = {
-          enable = true;
-          nssmdns4 = true;
-          openFirewall = true;
-        };
-      })
-
-      (lib.mkIf battery {
-        services.tlp.enable = true;
-        services.upower.enable = true;
-      })
-    ] ++ extraModules;
-  };
-}
+        boo
