7c90b27595993bc88fa1553813376ef855d51e28
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-11 02:08:52 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: {
     __functor = self: args @ {
       hostName ? "nixos",
@@ -34,34 +33,28 @@
       modules = [
         ({ ... }: {
           system.stateVersion = "24.11";
-          
           nixpkgs.config.allowUnfree = true;
           nix.settings = {
             experimental-features = [ "nix-command" "flakes" ];
             auto-optimise-store = true;
             warn-dirty = false;
           };
-
           time.timeZone = timeZone;
           i18n.defaultLocale = locale;
           console.keyMap = keyboardLayout;
-          
           networking = {
             networkmanager.enable = true;
             useDHCP = lib.mkDefault true;
             hostName = hostName;
           };
-          
           users.users.${userName} = {
             isNormalUser = true;
             extraGroups = [ "wheel" "networkmanager" ];
           };
-
           fileSystems."/" = {
             device = rootDevice;
             fsType = "ext4";
           };
-
           hardware = {
             enableAllFirmware = true;
             enableRedistributableFirmware = true;
@@ -71,25 +64,20 @@
             };
           };
         })
-
         (lib.mkIf (cpuVendor == "intel") {
           hardware.cpu.intel.updateMicrocode = true;
         })
-
         (lib.mkIf (cpuVendor == "amd") {
           hardware.cpu.amd.updateMicrocode = true;
         })
-
         (lib.mkIf (gpuVendor == "intel") {
           hardware.graphics.extraPackages = [ pkgs.intel-media-driver ];
           services.xserver.videoDrivers = [ "modesetting" ];
         })
-
         (lib.mkIf (gpuVendor == "amd") {
           hardware.amdgpu.initrd.enable = true;
           services.xserver.videoDrivers = [ "amdgpu" ];
         })
-
         (lib.mkIf (gpuVendor == "nvidia") {
           hardware.nvidia = {
             open = false;
@@ -100,7 +88,6 @@
           boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
           services.xserver.videoDrivers = [ "nvidia" ];
         })
-
         (lib.mkIf (bootDevice != null) {
           boot.loader.systemd-boot = {
             enable = true;
@@ -115,7 +102,6 @@
             fsType = "vfat";
           };
         })
-
         (lib.mkIf (bootDevice == null) {
           boot.loader.grub = {
             enable = true;
@@ -123,24 +109,20 @@
             efiSupport = false;
           };
         })
-
         (lib.mkIf (swapDevice != null) {
           swapDevices = [{ device = swapDevice; }];
         })
-
         (lib.mkIf disableNixApps {
           documentation.nixos.enable = false;
           services.xserver.excludePackages = [ pkgs.xterm ];
           environment.defaultPackages = [];
         })
-
         (lib.mkIf animateStartup {
           boot.plymouth = {
             enable = true;
             theme = "spinner";
           };
         })
-
         (lib.mkIf autoUpgrade {
           system.autoUpgrade = {
             enable = true;
@@ -148,7 +130,6 @@
             dates = "04:00";
           };
         })
-
         (lib.mkIf gamingTweaks {
           boot = {
             kernelPackages = pkgs.linuxPackages_xanmod;
@@ -160,7 +141,6 @@
             kernelParams = [ "mitigations=off" "nowatchdog" ];
           };
         })
-
         (lib.mkIf hiResAudio {
           services.pulseaudio.enable = false;
           security.rtkit.enable = true;
@@ -176,16 +156,13 @@
             };
           };
         })
-
         (lib.mkIf dualBoot {
           time.hardwareClockInLocalTime = true;
           boot.loader.grub.useOSProber = true;
         })
-
         (lib.mkIf gamepad {
           hardware.xpadneo.enable = true;
         })
-
         (lib.mkIf touchpad {
           services.xserver.libinput = {
             enable = true;
@@ -195,7 +172,6 @@
             };
           };
         })
-
         (lib.mkIf bluetooth {
           hardware.bluetooth = {
             enable = true;
@@ -203,7 +179,6 @@
           };
           services.blueman.enable = true;
         })
-
         (lib.mkIf printing {
           services.printing.enable = true;
           services.avahi = {
@@ -212,7 +187,6 @@
             openFirewall = true;
           };
         })
-
         (lib.mkIf battery {
           services.tlp.enable = true;
           services.upower.enable = true;
