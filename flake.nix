530f4c80962620e14218b88756a3895067b84f90
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-15 01:34:17 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
   outputs = { self, nixpkgs }: {
     __functor = self: args @ {
       stateVersion ? null,
